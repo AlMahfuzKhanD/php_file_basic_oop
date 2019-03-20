0942d3fdeb9cd56a38fa2418380da9fb0c09f194
mahfuz380 <almahfuz380@gmail.com>
2019-03-20 18:30:26 +0600

    file upload error done

--- upload.php
+++ upload.php
@@ -11,6 +11,19 @@ if(isset($_POST['submit'])){
 
     print_r($_FILES['file_upload']);
     echo "</pre>";
+
+    $upload_errors = array(
+        UPLOAD_ERR_OK => "There is no error",
+        UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesiz directive in php.ini",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+        UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded",
+        UPLOAD_ERR_NO_FILE => "No file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload."
+    );
+    $the_error = $_FILES['file_upload']['error']; // error what is catched in the file after being uploaded
+    $the_message = $upload_errors[$the_error];
 }
 
 
@@ -26,6 +39,18 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 <form action="upload.php" method="post" enctype="multipart/form-data">
+
+    <h2>
+
+        <?php
+        if(!empty($upload_errors)){
+            echo $the_message;
+        }
+
+        ?>
+
+    </h2>
+
     <input type="file" name="file_upload">
     <input type="submit" name="submit">
     
